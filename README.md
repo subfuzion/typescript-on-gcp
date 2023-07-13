407108a41927af159981a8edd00fa8efa91de98d
Tony Pujals <tonypujals@google.com>
2023-07-13 13:38:51 -0700

    add docs template

---
+++ README.md
@@ -0,0 +1,7 @@
+# TypeScript on GCP
+
+Get up and running with full stack development in TypeScript on Google Cloud Platform.
+
+## License
+
+This project is licensed under the [Apache 2.0 License](./LICENSE).
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>TypeScript on GCP</span>,
+  project: {
+    link: 'https://github.com/subfuzion/typescript-on-gcp',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/subfuzion/typescript-on-gcp',
+  footer: {
+    text: 'TypeScript on GCP - Copyright (c) 2023 Tony Pujals',
+  },
+}
+
+export default config
