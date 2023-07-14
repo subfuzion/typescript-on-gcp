06af4ad3d437c99fe225c438cafa5ade54752027
Tony Pujals <tonypujals@google.com>
2023-07-13 20:26:50 -0700

    wip

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/subfuzion/typescript-on-gcp',
   footer: {
-    text: 'TypeScript on GCP - Copyright (c) 2023 Tony Pujals',
+    text: 'TypeScript on GCP - Copyright (c) 2023 Tony Pujals. All rights reserved',
   },
 }
 
